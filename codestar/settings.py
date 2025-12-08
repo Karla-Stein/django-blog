46f16b02807d2a3f541ec407a169d828eb12de6a
k_stein <karlasteinbrink@gmail.com>
2025-12-08 13:01:13 +0000

    Uncomment predefined SECRET_KEY

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-u+$c3^@uv5v2^qcb*grhb(gfiyth_p$)ehy-6bxe@zdq7@owj0'  # noqa: E501
+SECRET_KEY = 'django-insecure-u+$c3^@uv5v2^qcb*grhb(gfiyth_p$)ehy-6bxe@zdq7@owj0'  # noqa: E501
 
 SECRET_KEY = {
     os.environ.get("SECRET_KEY")
