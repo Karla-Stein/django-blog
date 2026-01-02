618acedcd949582102b9bdb7b6f17f2ab8e6fbd3
k_stein <karlasteinbrink@gmail.com>
2026-01-02 15:17:05 +0100

    Enable serving of image files

--- static/js/comments.js
+++ static/js/comments.js
@@ -39,7 +39,7 @@ for (let button of editButtons) {
 * the user for confirmation before deletion.
 */
 for (let button of deleteButtons) {
-  button.addEventListener("click", (e) => {
+    button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
     deleteConfirm.href = `delete_comment/${commentId}`;
     deleteModal.show();
